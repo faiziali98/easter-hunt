9bb434b9255dec2218126904ae46c255dfcce77f
Faizan Safdar Ali <faiziali436@gmail.com>
2023-04-10 15:18:56 +0200

    Updated hint

--- door-1/routes/index.js
+++ door-1/routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   const paragraph = "Whats the answer to unlock door 2?";
-  res.render('index', { paragraph, hiddenData: "2" });
+  res.render('index', { paragraph, result:"567A0D1C92FEBB77", hiddenData: "2" });
 });
 
 module.exports = router;
